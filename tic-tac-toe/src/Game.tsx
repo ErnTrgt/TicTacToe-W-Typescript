0705398e6fd2c562f48e213effd901472ee8e79d
ErnTrgt <erntrgt10@gmail.com>
2024-02-29 01:08:49 +0300

    INIT

---
+++ tic-tac-toe/src/Game.tsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+import Square from "./Square";
+
+const INITIAL_GAME_STATE = ["X", "X", "", "X", "", "", "", "", ""];
+
+const Game = () => {
+    const [gameState, setGameState] = useState(INITIAL_GAME_STATE);
+
+    return (
+        <div className='h-full p-8 text-slate-800 bg-gradient-to-r from-cyan-500 to-blue-500'>
+            <h1 className='text-center text-5xl mb-4 font-display text-white'>
+                Tic-Tac-Toe Game
+            </h1>
+            <div>
+                <div className='grid grid-cols-3 gap-3 mx-auto w-96'>
+                    {gameState.map((player, index) => (
+                        <Square key={index}>{player}</Square>
+                    ))}
+                </div>
+                <div>SCORES</div>
+            </div>
+        </div>
+    );
+};
+
+export default Game;
---
+++ tic-tac-toe/src/Square.tsx
@@ -0,0 +1,15 @@
+import { ReactNode } from "react";
+
+type Props = {
+    children: ReactNode;
+};
+
+const Square = ({ children }: Props) => {
+    return (
+        <div className='h-36 border-solid border-4 border-slate-400 font-display text-7xl text-center flex justify-center items-center cursor-pointer'>
+            <span>{children}</span>
+        </div>
+    );
+};
+
+export default Square;
